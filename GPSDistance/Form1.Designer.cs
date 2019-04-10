28cbc86c32d2ff68a2bf0ba9c2b60e93248bc1ff
蔡旭昇 <ths@stu.edu.tw>
2019-04-10 09:48:20 +0800

    加入專案檔案。

---
+++ GPSDistance/Form1.Designer.cs
@@ -0,0 +1,425 @@
+﻿namespace GPSDistance
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// 設計工具所需的變數。
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// 清除任何使用中的資源。
+    /// </summary>
+    /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form 設計工具產生的程式碼
+
+    /// <summary>
+    /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+    /// 這個方法的內容。
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.button1 = new System.Windows.Forms.Button();
+      this.button2 = new System.Windows.Forms.Button();
+      this.progressBar1 = new System.Windows.Forms.ProgressBar();
+      this.progressBar2 = new System.Windows.Forms.ProgressBar();
+      this.groupBox1 = new System.Windows.Forms.GroupBox();
+      this.groupBox2 = new System.Windows.Forms.GroupBox();
+      this.button9 = new System.Windows.Forms.Button();
+      this.button8 = new System.Windows.Forms.Button();
+      this.button7 = new System.Windows.Forms.Button();
+      this.button6 = new System.Windows.Forms.Button();
+      this.button5 = new System.Windows.Forms.Button();
+      this.button4 = new System.Windows.Forms.Button();
+      this.button3 = new System.Windows.Forms.Button();
+      this.groupBox3 = new System.Windows.Forms.GroupBox();
+      this.button10 = new System.Windows.Forms.Button();
+      this.comboBox4 = new System.Windows.Forms.ComboBox();
+      this.comboBox3 = new System.Windows.Forms.ComboBox();
+      this.groupBox5 = new System.Windows.Forms.GroupBox();
+      this.textBox4 = new System.Windows.Forms.TextBox();
+      this.label6 = new System.Windows.Forms.Label();
+      this.textBox3 = new System.Windows.Forms.TextBox();
+      this.label5 = new System.Windows.Forms.Label();
+      this.textBox2 = new System.Windows.Forms.TextBox();
+      this.label4 = new System.Windows.Forms.Label();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.label3 = new System.Windows.Forms.Label();
+      this.comboBox2 = new System.Windows.Forms.ComboBox();
+      this.label2 = new System.Windows.Forms.Label();
+      this.comboBox1 = new System.Windows.Forms.ComboBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.groupBox4 = new System.Windows.Forms.GroupBox();
+      this.groupBox1.SuspendLayout();
+      this.groupBox2.SuspendLayout();
+      this.groupBox3.SuspendLayout();
+      this.groupBox5.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(6, 21);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(115, 23);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "計算站間距離";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // button2
+      // 
+      this.button2.Location = new System.Drawing.Point(6, 50);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(115, 23);
+      this.button2.TabIndex = 1;
+      this.button2.Text = "計算最短距離";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // progressBar1
+      // 
+      this.progressBar1.Location = new System.Drawing.Point(127, 21);
+      this.progressBar1.Name = "progressBar1";
+      this.progressBar1.Size = new System.Drawing.Size(100, 23);
+      this.progressBar1.TabIndex = 2;
+      // 
+      // progressBar2
+      // 
+      this.progressBar2.Location = new System.Drawing.Point(128, 50);
+      this.progressBar2.Name = "progressBar2";
+      this.progressBar2.Size = new System.Drawing.Size(100, 23);
+      this.progressBar2.TabIndex = 3;
+      this.progressBar2.UseWaitCursor = true;
+      // 
+      // groupBox1
+      // 
+      this.groupBox1.Controls.Add(this.button1);
+      this.groupBox1.Controls.Add(this.progressBar2);
+      this.groupBox1.Controls.Add(this.button2);
+      this.groupBox1.Controls.Add(this.progressBar1);
+      this.groupBox1.Location = new System.Drawing.Point(12, 12);
+      this.groupBox1.Name = "groupBox1";
+      this.groupBox1.Size = new System.Drawing.Size(241, 85);
+      this.groupBox1.TabIndex = 4;
+      this.groupBox1.TabStop = false;
+      this.groupBox1.Text = "建立預設資料";
+      // 
+      // groupBox2
+      // 
+      this.groupBox2.Controls.Add(this.button9);
+      this.groupBox2.Controls.Add(this.button8);
+      this.groupBox2.Controls.Add(this.button7);
+      this.groupBox2.Controls.Add(this.button6);
+      this.groupBox2.Controls.Add(this.button5);
+      this.groupBox2.Controls.Add(this.button4);
+      this.groupBox2.Controls.Add(this.button3);
+      this.groupBox2.Location = new System.Drawing.Point(12, 103);
+      this.groupBox2.Name = "groupBox2";
+      this.groupBox2.Size = new System.Drawing.Size(241, 335);
+      this.groupBox2.TabIndex = 5;
+      this.groupBox2.TabStop = false;
+      this.groupBox2.Text = "莫斯科地鐵介紹";
+      // 
+      // button9
+      // 
+      this.button9.Location = new System.Drawing.Point(7, 195);
+      this.button9.Name = "button9";
+      this.button9.Size = new System.Drawing.Size(220, 23);
+      this.button9.TabIndex = 7;
+      this.button9.Text = "優惠資訊";
+      this.button9.UseVisualStyleBackColor = true;
+      // 
+      // button8
+      // 
+      this.button8.Location = new System.Drawing.Point(7, 166);
+      this.button8.Name = "button8";
+      this.button8.Size = new System.Drawing.Size(220, 23);
+      this.button8.TabIndex = 6;
+      this.button8.Text = "票價資訊";
+      this.button8.UseVisualStyleBackColor = true;
+      // 
+      // button7
+      // 
+      this.button7.Location = new System.Drawing.Point(7, 137);
+      this.button7.Name = "button7";
+      this.button7.Size = new System.Drawing.Size(220, 23);
+      this.button7.TabIndex = 5;
+      this.button7.Text = "列車介紹";
+      this.button7.UseVisualStyleBackColor = true;
+      // 
+      // button6
+      // 
+      this.button6.Location = new System.Drawing.Point(7, 108);
+      this.button6.Name = "button6";
+      this.button6.Size = new System.Drawing.Size(220, 23);
+      this.button6.TabIndex = 4;
+      this.button6.Text = "各地鐵站介紹";
+      this.button6.UseVisualStyleBackColor = true;
+      // 
+      // button5
+      // 
+      this.button5.Location = new System.Drawing.Point(7, 79);
+      this.button5.Name = "button5";
+      this.button5.Size = new System.Drawing.Size(220, 23);
+      this.button5.TabIndex = 3;
+      this.button5.Text = "行車資訊介紹";
+      this.button5.UseVisualStyleBackColor = true;
+      // 
+      // button4
+      // 
+      this.button4.Location = new System.Drawing.Point(6, 50);
+      this.button4.Name = "button4";
+      this.button4.Size = new System.Drawing.Size(220, 23);
+      this.button4.TabIndex = 2;
+      this.button4.Text = "各路線介紹";
+      this.button4.UseVisualStyleBackColor = true;
+      // 
+      // button3
+      // 
+      this.button3.Location = new System.Drawing.Point(6, 21);
+      this.button3.Name = "button3";
+      this.button3.Size = new System.Drawing.Size(220, 23);
+      this.button3.TabIndex = 1;
+      this.button3.Text = "莫斯科地鐵發展歷史";
+      this.button3.UseVisualStyleBackColor = true;
+      // 
+      // groupBox3
+      // 
+      this.groupBox3.Controls.Add(this.button10);
+      this.groupBox3.Controls.Add(this.comboBox4);
+      this.groupBox3.Controls.Add(this.comboBox3);
+      this.groupBox3.Controls.Add(this.groupBox5);
+      this.groupBox3.Controls.Add(this.comboBox2);
+      this.groupBox3.Controls.Add(this.label2);
+      this.groupBox3.Controls.Add(this.comboBox1);
+      this.groupBox3.Controls.Add(this.label1);
+      this.groupBox3.Location = new System.Drawing.Point(259, 12);
+      this.groupBox3.Name = "groupBox3";
+      this.groupBox3.Size = new System.Drawing.Size(323, 426);
+      this.groupBox3.TabIndex = 6;
+      this.groupBox3.TabStop = false;
+      this.groupBox3.Text = "莫斯科地鐵搭乘查詢";
+      // 
+      // button10
+      // 
+      this.button10.Location = new System.Drawing.Point(75, 74);
+      this.button10.Name = "button10";
+      this.button10.Size = new System.Drawing.Size(242, 23);
+      this.button10.TabIndex = 7;
+      this.button10.Text = "查詢";
+      this.button10.UseVisualStyleBackColor = true;
+      this.button10.Click += new System.EventHandler(this.button10_Click);
+      // 
+      // comboBox4
+      // 
+      this.comboBox4.FormattingEnabled = true;
+      this.comboBox4.Location = new System.Drawing.Point(222, 47);
+      this.comboBox4.Name = "comboBox4";
+      this.comboBox4.Size = new System.Drawing.Size(95, 20);
+      this.comboBox4.TabIndex = 6;
+      this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+      // 
+      // comboBox3
+      // 
+      this.comboBox3.FormattingEnabled = true;
+      this.comboBox3.Location = new System.Drawing.Point(222, 23);
+      this.comboBox3.Name = "comboBox3";
+      this.comboBox3.Size = new System.Drawing.Size(95, 20);
+      this.comboBox3.TabIndex = 4;
+      this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+      // 
+      // groupBox5
+      // 
+      this.groupBox5.Controls.Add(this.textBox4);
+      this.groupBox5.Controls.Add(this.label6);
+      this.groupBox5.Controls.Add(this.textBox3);
+      this.groupBox5.Controls.Add(this.label5);
+      this.groupBox5.Controls.Add(this.textBox2);
+      this.groupBox5.Controls.Add(this.label4);
+      this.groupBox5.Controls.Add(this.textBox1);
+      this.groupBox5.Controls.Add(this.label3);
+      this.groupBox5.Location = new System.Drawing.Point(9, 91);
+      this.groupBox5.Name = "groupBox5";
+      this.groupBox5.Size = new System.Drawing.Size(308, 236);
+      this.groupBox5.TabIndex = 3;
+      this.groupBox5.TabStop = false;
+      this.groupBox5.Text = "查詢結果";
+      this.groupBox5.Visible = false;
+      // 
+      // textBox4
+      // 
+      this.textBox4.Location = new System.Drawing.Point(9, 124);
+      this.textBox4.Multiline = true;
+      this.textBox4.Name = "textBox4";
+      this.textBox4.Size = new System.Drawing.Size(293, 106);
+      this.textBox4.TabIndex = 7;
+      // 
+      // label6
+      // 
+      this.label6.AutoSize = true;
+      this.label6.Location = new System.Drawing.Point(7, 108);
+      this.label6.Name = "label6";
+      this.label6.Size = new System.Drawing.Size(53, 12);
+      this.label6.TabIndex = 6;
+      this.label6.Text = "轉乘資訊";
+      // 
+      // textBox3
+      // 
+      this.textBox3.Location = new System.Drawing.Point(66, 74);
+      this.textBox3.Name = "textBox3";
+      this.textBox3.ReadOnly = true;
+      this.textBox3.Size = new System.Drawing.Size(236, 22);
+      this.textBox3.TabIndex = 5;
+      // 
+      // label5
+      // 
+      this.label5.AutoSize = true;
+      this.label5.Location = new System.Drawing.Point(7, 78);
+      this.label5.Name = "label5";
+      this.label5.Size = new System.Drawing.Size(61, 12);
+      this.label5.TabIndex = 4;
+      this.label5.Text = "距離(公尺)";
+      // 
+      // textBox2
+      // 
+      this.textBox2.Location = new System.Drawing.Point(66, 46);
+      this.textBox2.Name = "textBox2";
+      this.textBox2.ReadOnly = true;
+      this.textBox2.Size = new System.Drawing.Size(236, 22);
+      this.textBox2.TabIndex = 3;
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.Location = new System.Drawing.Point(7, 50);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(61, 12);
+      this.label4.TabIndex = 2;
+      this.label4.Text = "費用(盧布)";
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(66, 18);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.ReadOnly = true;
+      this.textBox1.Size = new System.Drawing.Size(236, 22);
+      this.textBox1.TabIndex = 1;
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(7, 22);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(61, 12);
+      this.label3.TabIndex = 0;
+      this.label3.Text = "時間(分鐘)";
+      // 
+      // comboBox2
+      // 
+      this.comboBox2.FormattingEnabled = true;
+      this.comboBox2.Location = new System.Drawing.Point(75, 47);
+      this.comboBox2.Name = "comboBox2";
+      this.comboBox2.Size = new System.Drawing.Size(141, 20);
+      this.comboBox2.TabIndex = 1;
+      // 
+      // label2
+      // 
+      this.label2.Location = new System.Drawing.Point(16, 50);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(53, 17);
+      this.label2.TabIndex = 2;
+      this.label2.Text = "到達站";
+      this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+      // 
+      // comboBox1
+      // 
+      this.comboBox1.FormattingEnabled = true;
+      this.comboBox1.Location = new System.Drawing.Point(75, 21);
+      this.comboBox1.Name = "comboBox1";
+      this.comboBox1.Size = new System.Drawing.Size(141, 20);
+      this.comboBox1.TabIndex = 0;
+      // 
+      // label1
+      // 
+      this.label1.Location = new System.Drawing.Point(16, 24);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(53, 17);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "出發站";
+      this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+      // 
+      // groupBox4
+      // 
+      this.groupBox4.Location = new System.Drawing.Point(588, 12);
+      this.groupBox4.Name = "groupBox4";
+      this.groupBox4.Size = new System.Drawing.Size(200, 426);
+      this.groupBox4.TabIndex = 7;
+      this.groupBox4.TabStop = false;
+      this.groupBox4.Text = "莫斯科地鐵購票功能";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.groupBox4);
+      this.Controls.Add(this.groupBox3);
+      this.Controls.Add(this.groupBox2);
+      this.Controls.Add(this.groupBox1);
+      this.Name = "Form1";
+      this.Text = "莫斯科地鐵資訊系統";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.groupBox1.ResumeLayout(false);
+      this.groupBox2.ResumeLayout(false);
+      this.groupBox3.ResumeLayout(false);
+      this.groupBox5.ResumeLayout(false);
+      this.groupBox5.PerformLayout();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.ProgressBar progressBar1;
+    private System.Windows.Forms.ProgressBar progressBar2;
+    private System.Windows.Forms.GroupBox groupBox1;
+    private System.Windows.Forms.GroupBox groupBox2;
+    private System.Windows.Forms.Button button9;
+    private System.Windows.Forms.Button button8;
+    private System.Windows.Forms.Button button7;
+    private System.Windows.Forms.Button button6;
+    private System.Windows.Forms.Button button5;
+    private System.Windows.Forms.Button button4;
+    private System.Windows.Forms.Button button3;
+    private System.Windows.Forms.GroupBox groupBox3;
+    private System.Windows.Forms.GroupBox groupBox4;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.GroupBox groupBox5;
+    private System.Windows.Forms.TextBox textBox3;
+    private System.Windows.Forms.Label label5;
+    private System.Windows.Forms.TextBox textBox2;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.ComboBox comboBox2;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.ComboBox comboBox1;
+    private System.Windows.Forms.ComboBox comboBox4;
+    private System.Windows.Forms.ComboBox comboBox3;
+    private System.Windows.Forms.TextBox textBox4;
+    private System.Windows.Forms.Label label6;
+    private System.Windows.Forms.Button button10;
+  }
+}
+
---
+++ GPSDistance/Form1.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections; // ArrayList
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Device.Location; // GeoCoordinate
+using System.Data.Entity; // ToBindingList()
+
+namespace GPSDistance
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    // 計算站間距離
+    private void button1_Click(object sender, EventArgs e)
+    {
+      using (var me = new MetroEntities())
+      {
+        // 取出LineEdge所有Edge
+        var edges =
+          from edge in me.LineEdge
+          join bs in me.車站 on edge.起站編號 equals bs.車站編號
+          join es in me.車站 on edge.迄站編號 equals es.車站編號
+          select new
+          {
+            edge.Edge編號,
+            經度1 = bs.經度,
+            緯度1 = bs.緯度,
+            經度2 = es.經度,
+            緯度2 = es.緯度
+          };
+
+        progressBar1.Minimum = 1;
+        progressBar1.Maximum = edges.Count();
+        progressBar1.Step = 1;
+
+        // 計算每個Edge的距離
+        foreach (var le in edges)
+        {
+          var gc1 = new GeoCoordinate(Convert.ToDouble(le.緯度1), Convert.ToDouble(le.經度1));
+          var gc2 = new GeoCoordinate(Convert.ToDouble(le.緯度2), Convert.ToDouble(le.經度2));
+          var edge = me.LineEdge.Find(le.Edge編號);
+          edge.距離 = Convert.ToDecimal(gc1.GetDistanceTo(gc2));
+
+          progressBar1.PerformStep();
+        }
+        me.SaveChanges();
+      }
+
+      MessageBox.Show("計算結束!");
+    }
+
+    // 取得最短距離節點編號
+    private int MinimumDistance(int[] distance, bool[] shortestPathTreeSet, int verticesCount)
+    {
+      int min = int.MaxValue;
+      int minIndex = 0;
+
+      for (int v = 0; v < verticesCount; v++)
+      {
+        if (shortestPathTreeSet[v] == false && distance[v] <= min)
+        {
+          min = distance[v];
+          minIndex = v;
+        }
+      }
+
+      return minIndex;
+    }
+
+    // Dijkstra演算法+紀錄路徑 
+    private void Dijkstra(int[,] graph, int source, int verticesCount, ref int[] distance, ref int[] previous)
+    {
+      bool[] shortestPathTreeSet = new bool[verticesCount]; // 節點集合
+
+      for (int i = 0; i < verticesCount; i++)
+      {
+        distance[i] = int.MaxValue; // 初始化距離為最大值
+        previous[i] = -1; // 初始化紀錄路徑
+        shortestPathTreeSet[i] = false;
+      }
+
+      distance[source] = 0;
+      for (int count = 0; count < verticesCount - 1; count++)
+      {
+        int u = MinimumDistance(distance, shortestPathTreeSet, verticesCount);
+        shortestPathTreeSet[u] = true; // 加入最短路徑集合
+
+        for (int v = 0; v < verticesCount; v++) // 修正最短路徑
+        {
+          if (!shortestPathTreeSet[v] &&
+            graph[u, v] < int.MaxValue &&
+            distance[u] < int.MaxValue &&
+            distance[u] + graph[u, v] < distance[v])
+          {
+            distance[v] = distance[u] + graph[u, v];
+            previous[v] = u;
+          }
+        }
+      }
+    }
+
+    // 計算最短距離
+    private void button2_Click(object sender, EventArgs e)
+    {
+      using (var me = new MetroEntities())
+      {
+        // 清空資料表
+        me.Database.ExecuteSqlCommand("DELETE FROM Dijkstra");
+        me.Database.ExecuteSqlCommand("DELETE FROM Trip");
+        me.Database.ExecuteSqlCommand("DELETE FROM TripNode");
+
+        var verticesCount = me.車站.Count();
+        int[,] graph = new int[verticesCount, verticesCount];
+        var trips = new ArrayList();
+
+        progressBar2.Minimum = 1;
+        progressBar2.Maximum = verticesCount;
+        progressBar2.Step = 1;
+
+        // 初始化路網
+        for (int i = 0; i < verticesCount; i++)
+        {
+          trips.Add(new List<int>());
+          for (int j = 0; j < verticesCount; j++)
+          {
+            graph[i, j] = int.MaxValue;
+
+            var trip = new Trip();
+            trip.起站編號 = i + 1;
+            trip.迄站編號 = j + 1;
+            me.Trip.Add(trip);
+          }
+        }
+        me.SaveChanges(); // 提早取得Trip編號
+        foreach (var edge in me.LineEdge)
+        {
+          graph[edge.起站編號 - 1, edge.迄站編號 - 1] = Convert.ToInt32(edge.距離);
+        }
+        foreach (var edge in me.轉乘)
+        {
+          graph[edge.起站編號 - 1, edge.迄站編號 - 1] = 0;
+        }
+
+        int[] distance = new int[verticesCount];
+        int[] previous = new int[verticesCount];
+        for (int i = 0; i < verticesCount; i++)
+        {
+          Dijkstra(graph, i, verticesCount, ref distance, ref previous);
+          for (int j = i; j < verticesCount; j++)
+          {
+            var d = new Dijkstra();
+            d.起站編號 = i + 1;
+            d.迄站編號 = j + 1;
+            d.最短距離 = Convert.ToDecimal(distance[j]);
+            me.Dijkstra.Add(d);
+            d.起站編號 = j + 1;
+            d.迄站編號 = i + 1;
+            d.最短距離 = Convert.ToDecimal(distance[j]);
+            me.Dijkstra.Add(d);
+          }
+
+          // 取出最短路徑
+          for (int j = verticesCount - 1; j >= 0; j--)
+          {
+            var trip = (List<int>)trips[j];
+            trip.Clear();
+            var p = j;
+            while (p >= 0)
+            {
+              trip.Add(p + 1);
+              p = previous[p];
+            }
+            trip.Reverse();
+
+            // 取得Trip編號
+            var path = me.Trip.Where((o) => o.起站編號 == i + 1 && o.迄站編號 == j + 1).Single();
+            string line = "";
+            var tn = from t in trip // 取得線路編號
+                     join s in me.車站 on t equals s.車站編號
+                     select new { lid = s.線路編號, sid = s.車站編號 };
+            var nlist = tn.ToList();
+            for (int k = 0, l = 0; k < nlist.Count(); k++)
+            {
+              if (k == 0 || k == nlist.Count() - 1) // 起迄站不用轉乘
+              {
+                var te = new TripNode();
+                te.Trip編號 = path.Trip編號;
+                te.序號 = l++;
+                te.車站編號 = nlist[k].sid;
+                te.是否轉乘 = false;
+                me.TripNode.Add(te);
+                line = nlist[k].lid;
+              }
+              else if (nlist[k].lid != line) // 換線必須轉乘
+              {
+                var te = new TripNode();
+                te.Trip編號 = path.Trip編號;
+                te.序號 = l++;
+                te.車站編號 = nlist[k - 1].sid;
+                te.是否轉乘 = true;
+                me.TripNode.Add(te);
+                te = new TripNode();
+                te.Trip編號 = path.Trip編號;
+                te.序號 = l++;
+                te.車站編號 = nlist[k].sid;
+                te.是否轉乘 = true;
+                me.TripNode.Add(te);
+                line = nlist[k].lid;
+              }
+            }
+          }
+
+          progressBar2.PerformStep();
+          me.SaveChanges();
+        }
+      }
+
+      MessageBox.Show("計算結束");
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      using (var me = new MetroEntities())
+      {
+        comboBox3.DataSource = me.線路.ToList();
+        comboBox3.DisplayMember = "中文名稱";
+        comboBox3.ValueMember = "線路編號";
+        comboBox4.DataSource = me.線路.ToList();
+        comboBox4.DisplayMember = "中文名稱";
+        comboBox4.ValueMember = "線路編號";
+      }
+    }
+
+    private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+    {
+      using (var me = new MetroEntities())
+      {
+        var 起站 =
+          from o in me.車站
+          where o.線路編號 == ((線路)comboBox3.SelectedItem).線路編號
+          select o;
+        comboBox1.DataSource = 起站.ToList();
+        comboBox1.DisplayMember = "中文名稱";
+        comboBox1.ValueMember = "車站編號";
+      }
+
+    }
+
+    private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+    {
+      using (var me = new MetroEntities())
+      {
+        var 迄站 =
+          from o in me.車站
+          where o.線路編號 == ((線路)comboBox4.SelectedItem).線路編號
+          select o;
+        comboBox2.DataSource = 迄站.ToList();
+        comboBox2.DisplayMember = "中文名稱";
+        comboBox2.ValueMember = "車站編號";
+      }
+
+    }
+
+    private void button10_Click(object sender, EventArgs e)
+    {
+      using (var me = new MetroEntities())
+      {
+        int 起站 = Convert.ToInt32(comboBox1.SelectedValue);
+        int 迄站 = Convert.ToInt32(comboBox2.SelectedValue);
+        var 起訖距離 =
+          (from o in me.Dijkstra
+          where o.起站編號 == 起站 && o.迄站編號 == 迄站
+          select o).First();
+        double time = (double)起訖距離.最短距離 / 1000 / 39.54 * 60; // 平均時速 39.54km/h
+        textBox1.Text = time.ToString("F0"); 
+        textBox2.Text = "55";
+        textBox3.Text = 起訖距離.最短距離.ToString();
+
+        var 起訖路徑 =
+          from o in me.Trip
+          join p in me.TripNode on o.Trip編號 equals p.Trip編號
+          join s in me.車站 on p.車站編號 equals s.車站編號
+          join l in me.線路 on s.線路編號 equals l.線路編號
+          where o.起站編號 == 起站 && o.迄站編號 == 迄站
+          orderby p.Trip編號, p.序號
+          select new { 搭乘 = o.Trip編號, 線路 = l.中文名稱, 車站 = s.中文名稱, 轉乘 = p.是否轉乘 };
+        textBox4.Text = "";
+        long trip = -1;
+        int i = 1;
+        foreach (var p in 起訖路徑)
+        {
+          if (p.搭乘 != trip)
+          {
+            trip = p.搭乘;
+            textBox4.Text += "第" + i++ + "種：\r\n";
+          }
+          textBox4.Text += ((bool)p.轉乘 ? "轉＞" : "搭＞") + p.線路 + "(" + p.車站 + ")\r\n";
+        }
+        groupBox5.Visible = true;
+      }
+    }
+  }
+}
